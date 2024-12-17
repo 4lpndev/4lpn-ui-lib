c9bd23fb496a7eb49a819e250bb9256d6e095073
4lpndev <urdad995@gmail.com>
2024-12-17 22:36:09 +0100

    demo fix

--- demo.js
+++ demo.js
@@ -8,8 +8,11 @@ IMPORTANT: if you want to customize stuff, put before the "ui._theme = uiTheme.g
 example: uiTheme._theme.gui.backgroundColor = "purple"
 refere to the theme structure in docs
 */
+uiTheme._theme.tab_ctx.borderColor = "none"
 ui._theme = uiTheme.get_theme()
 let th = ui._theme
+
+let prevTab = document.createElement("a")
 
 //for debugging
 dbg.info("this is an information")
@@ -22,36 +25,38 @@ ui.create_window("<span style='color: red'>4</span>lpn's UI lib demo", ["280px",
 //for the tabs
 ui.create_tab_ctx(["0px", "-20px"]) //tab context
 const tab1 = ui.create_tab("aimbot", ["106px","60px"])
-const tab2 = ui.create_tab("aimbot", ["106px","60px"])
-const tab3 = ui.create_tab("aimbot", ["106px","60px"])
-const tab4 = ui.create_tab("aimbot", ["106px","60px"])
-const tab5 = ui.create_tab("aimbot", ["106px","60px"])
+const tab2 = ui.create_tab("misc", ["106px","60px"])
+const tab3 = ui.create_tab("visuals", ["106px","60px"])
+const tab4 = ui.create_tab("movement", ["106px","60px"])
 ui.append_tab_ctx() //append tabs to window
 
 ui.append_window() //render window to main screen
 
 //events and stuff
 tab1.addEventListener("click", () => {
-    ui.unload_tab() //add this before any actions
+    ui.unload_tab(prevTab) //add this before any actions
+    tab1.style.backgroundColor = th.tabs.selectedColor
     const label = ui.create_label("this is tab 1", ["20px","50px"])
+    prevTab = tab1
 })
 
 tab2.addEventListener("click", () => {
-    ui.unload_tab()
+    ui.unload_tab(prevTab)
+    tab2.style.backgroundColor = th.tabs.selectedColor
     const label = ui.create_label("this is tab 2", ["20px","50px"])
+    prevTab = tab2
 })
 
 tab3.addEventListener("click", () => {
-    ui.unload_tab()
+    ui.unload_tab(prevTab)
+    tab3.style.backgroundColor = th.tabs.selectedColor
     const label = ui.create_label("this is tab 3", ["20px","50px"])
+    prevTab = tab3
 })
 
 tab4.addEventListener("click", () => {
-    ui.unload_tab()
+    ui.unload_tab(prevTab)
+    tab4.style.backgroundColor = th.tabs.selectedColor
     const label = ui.create_label("this is tab 4", ["20px","50px"])
-})
-
-tab5.addEventListener("click", () => {
-    ui.unload_tab()
-    const label = ui.create_label("this is tab 5", ["20px","50px"])
+    prevTab = tab4
 })
