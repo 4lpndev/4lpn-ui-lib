83dc8dd056dcff9b43bf3b1e9919eece35f804bc
4lpndev <urdad995@gmail.com>
2026-02-02 18:42:51 +0100

    fixed main demo

--- demo.js
+++ demo.js
@@ -9,7 +9,7 @@ example: uiTheme._theme.gui.backgroundColor = "purple"
 refere to the theme structure in docs
 */
 uiTheme._theme.tab_ctx.borderColor = "none"
-ui._theme = uiTheme.get_theme()
+ui._theme = uiTheme.get_theme("default") //use default so it loads the default theme
 let th = ui._theme
 
 //let prevTab = document.createElement("a")
